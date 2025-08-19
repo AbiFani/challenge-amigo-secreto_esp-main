0ee73efe52ed17e0e6a431b1df3aceea730e9001
AbiFani <143127895+AbiFani@users.noreply.github.com>
2025-08-19 08:00:34 -0600

    se modifico el app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//crear un array para almacenar los nombres
+
+let listadeAmigos = [];
+let nombreSecreto = 0;
+let documentoHTML = 0;
+
+//implementa una funcion para agregar amigos
+function asignarNombre(){
+     let nombre = document.querySelector('nombre');
+     nombre.innerHTML= "text";
+     return;
+}
+
+function verificarIntento(){
+    let nombreIngresado = document.getElementById('amigo');
+
+     if(nombreIngresado == nombreSecreto){
+          console.log(nombreIngresado);
+            alert('por favor inserte un nombre:');
+            
+          
+     }else{
+         listadeAmigos.push(amigo); 
+     }
+       
+}
+
+//implementa una funcion para actualizar la lista de amigos
+
+function actualizarLista(){
+     listadeAmigos = document.getElementById('amigo');
+
+}
+//limpiar la listadeAmigos
+
+function limpiarLista(){
+     document.querySelector('#amigo').value = '';
+}
+//implementa una funcion para sortear amigos
+function SortearAmigo(){
+     if(listadeAmigos = vacia){
+          listadeAmigos = Math.floor
+
+     }
+
+}
+
+//mostrar resultado
+function mostrarResultado(){
+     documentoHTML = document.querySelector('amigo');
+    elementoHTML.innerHTML = texto;
+    return;
+     
+}
+
+
+
+
